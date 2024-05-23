9534c1af11d293087557ce60e963e91b6705badd
Maite <martinezf.maite@gmail.com>
2024-05-22 22:43:03 -0300

    Adds scale on logo

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-import { motion } from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 import logo from '../assets/framer-motion-logo.png';
 
 const Page1 = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  })
+
+  const scale = useTransform(scrollYProgress, [0, 0.5, 1], [0.5, 1, 0.5]);
+
   return (
     <div className='pageContainer flex items-center justify-center'>
       <div className='flex items-center justify-center'>
         <motion.div
+          ref={ref}
           initial={{ x: '-100%', opacity: 0 }}
           animate={{ x: '0%', opacity: 1 }}
           transition={{ duration: 1.5, type: "spring" }}
+          style={{ scale }}
         >
           <img src={logo} alt='' />
         </motion.div>
